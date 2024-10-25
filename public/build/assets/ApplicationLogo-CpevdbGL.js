e5a5b91a45fac19fe74c0604fdeef86f018126c0
Fijunas <abdulraof628@gmail.com>
2024-10-25 17:21:56 +0800

    remove node_modules in gitignore

---
+++ public/build/assets/ApplicationLogo-CpevdbGL.js
@@ -0,0 +1,1 @@
+import{Q as e,o as n,f as c}from"./app-OGxbsp_V.js";const s={},r=["href"];function a(o,t){return n(),c("img",{href:o.route("dashboard"),src:"/images/anf-logo-main-2x.png",class:"block select-none",alt:""},null,8,r)}const f=e(s,[["render",a]]);export{f as A};
