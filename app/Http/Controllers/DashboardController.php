ac227aa095251801beb868b800753c9ee493279d
Jarakkal's PC <alfaandfriends@github.com>
2025-05-09 16:48:14 +0800

    fix ob flush

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -179,6 +179,9 @@ class DashboardController extends Controller
                 ')
                 ->withMessages($messages)
                 ->asStream();
+
+            ob_start();
+            
             return response()->stream(function () use ($output) {
                 foreach ($output as $chunk) {
                     echo $chunk->text;
