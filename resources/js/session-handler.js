15a184d17c7a3f5a89f85daaf2f162b4be738914
Jarakkal's PC <alfaandfriends@github.com>
2025-05-15 16:12:53 +0800

    added logout timer

--- resources/js/session-handler.js
+++ resources/js/session-handler.js
@@ -25,11 +25,22 @@ document.addEventListener('scroll', resetIdleTimer);
 // Start the timer
 resetIdleTimer();
 
-// Handle AJAX session expiration
+// Handle AJAX session expiration for jQuery
 $.ajaxSetup({
     statusCode: {
         401: function() {
             window.location.href = '/login';
         }
     }
-}); 
+});
+
+// Handle Axios session expiration
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+); 
